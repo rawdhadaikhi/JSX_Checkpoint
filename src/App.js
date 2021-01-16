064020b775b86f61c5cbd562367ad0e362245ac0
rawdhadaikhi <raoudhadayki@gmail.com>
2021-01-16 16:23:08 +0100

    first commit jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+
+
 import './App.css';
+import './assets/style.css' ;
+import myimg from './imageInSrc.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appjsx">
+     <h1 className={"title red"}> I'm raoudha dayki</h1>
+      <br></br>
+      <img src={myimg} alt="bootstraplogo" />
+      <br></br>
+      <img src="/imageInPublic.png" alt="jslogo" />
+       <br></br>
+      
+      <video style={{width:320,height:240}} controls>
+       <source src="myvedio.mp4" type="video/mp4"></source>
+       </video>
     </div>
+   
+   
   );
+
 }
 
-export default App;
+export default App ;
+
